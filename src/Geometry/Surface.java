182b67b6fb3fcd78281e65e0121a15c7d9e00d8d
gr1ffin <89878211+gr1ffin@users.noreply.github.com>
2021-10-22 08:50:34 -0400

    Surface Final
    - finished surface functions
    - might update volume with more
    - time for algebra :frowning:

--- src/Geometry/Surface.java
+++ src/Geometry/Surface.java
@@ -1,4 +1,5 @@
 package Geometry;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -13,54 +14,103 @@ public class Surface {
             while (true) {
                 Scanner numberInput = new Scanner(System.in);
                 System.out.println("What is the length of the rectangular prism?");
-                String length = numberInput.nextLine();
+                double length = Double.parseDouble(numberInput.nextLine());
                 System.out.println("What is the width of the rectangular prism?");
-                String width = numberInput.nextLine();
+                double width = Double.parseDouble(numberInput.nextLine());
                 System.out.println("What is the height of the rectangular prism?");
-                String height = numberInput.nextLine();
+                double height = Double.parseDouble(numberInput.nextLine());
                 System.out.println("Your selected values were \nHeight: " + height + "\nLength: " + length + "\nWidth: " + width + "\nIs that correct? (Y/N)");
                 Scanner checkInput = new Scanner(System.in);
                 String correctString = checkInput.nextLine().toUpperCase(Locale.ROOT);
                 if (correctString.equals("Y")) {
-                    System.out.println("The area of the rectangular prism is: " + 2 * (Integer.parseInt(length) * Integer.parseInt(height) * Integer.parseInt(width)));
+                    System.out.println("The area of the rectangular prism is: " + 2 * length * height * width);
                     break;
                 } else if (correctString.equals("N")) {
                     System.out.println("Ok, let's try again.");
-                    continue;
                 } else {
                     System.out.println("Invalid input, please try again.");
-                    continue;
                 }
             }
         }
         else if (takeInput.equals("SPHERE")) {
-
+            while (true) {
+                Scanner numberInput = new Scanner(System.in);
+                System.out.println("What is the radius of the base of your object?");
+                double radius = Double.parseDouble(numberInput.nextLine());
+                System.out.println("Your selected values were \nRadius: " + radius + "\nIs that correct? (Y/N)");
+                Scanner checkInput = new Scanner(System.in);
+                String correctString = checkInput.nextLine().toUpperCase(Locale.ROOT);
+                if (correctString.equals("Y")) {
+                    System.out.println("The volume of the sphere is: " + 4 * Math.PI * Math.pow(radius, 3));
+                    break;
+                } else if (correctString.equals("N")) {
+                    System.out.println("Ok, let's try again.");
+                } else {
+                    System.out.println("Invalid input, please try again.");
+                }
+            }
         }
         else if (takeInput.equals("CYLINDER")) {
+            while (true) {
+                Scanner numberInput = new Scanner(System.in);
+                System.out.println("What is the radius of the base of your object?");
+                double radius = Double.parseDouble(numberInput.nextLine());
+                System.out.println("Your selected values were \nRadius: " + radius + "\nIs that correct? (Y/N)");
+                Scanner checkInput = new Scanner(System.in);
+                String correctString = checkInput.nextLine().toUpperCase(Locale.ROOT);
+                if (correctString.equals("Y")) {
+                    System.out.println("The volume of the cylinder is: " + 4 * Math.PI * Math.pow(radius, 2));
+                    break;
+                } else if (correctString.equals("N")) {
+                    System.out.println("Ok, let's try again.");
+                } else {
+                    System.out.println("Invalid input, please try again.");
+                }
 
+            }
         }
         else if (takeInput.equals("TRIANGULAR PRISM")) {
+            while(true) {
+                Scanner numberInput = new Scanner(System.in);
+                System.out.println("What is the length of the triangular prism?");
+                double length = Double.parseDouble(numberInput.nextLine());
+                System.out.println("What is the base of the triangular prism?");
+                double base = Double.parseDouble(numberInput.nextLine());
+                System.out.println("What is the height of the triangular prism?");
+                double height = Double.parseDouble(numberInput.nextLine());
+                System.out.println("What is the side of the triangular prism?");
+                double side = Double.parseDouble(numberInput.nextLine());
+                System.out.println("Your selected values were \nLength: " + length + "\nHeight: " + height + "\nBase: " + base + "\nSide: " + side + "\nIs that correct? (Y/N)");
+                Scanner checkInput = new Scanner(System.in);
+                String correctString = checkInput.nextLine().toUpperCase(Locale.ROOT);
+                if (correctString.equals("Y")) {
+                    System.out.println("The area of the triangular prism is: " + (base * height) + (2 * length * side) + (length * base));
+                    break;
+                } else if (correctString.equals("N")) {
+                    System.out.println("Ok, let's try again.");
+                } else {
+                    System.out.println("Invalid input, please try again.");
+                }
 
+            }
         }
         else if (takeInput.equals("PYRAMID")) {
             while (true) {
                 Scanner numberInput = new Scanner(System.in);
                 System.out.println("What is the base of the pyramid??");
-                String base = numberInput.nextLine();
+                double base = Double.parseDouble(numberInput.nextLine());
                 System.out.println("What us the side length of the pyramid?");
-                String side = numberInput.nextLine();
+                double side = Double.parseDouble(numberInput.nextLine());
                 System.out.println("Your selected values were \nBase: " + base + "\nSide: " + side + "\nIs that correct? (Y/N)");
                 Scanner checkInput = new Scanner(System.in);
                 String correctString = checkInput.nextLine().toUpperCase(Locale.ROOT);
                 if (correctString.equals("Y")) {
-                    System.out.println("The volume of the Pyramid is: " + Math.pow(Integer.parseInt(base), 2) + (2 * Integer.parseInt(base) * Integer.parseInt(side)));
+                    System.out.println("The volume of the Pyramid is: " + Math.pow(base, 2) + (2 * base) * side);
                     break;
                 } else if (correctString.equals("N")) {
                     System.out.println("Ok, let's try again.");
-                    continue;
                 } else {
                     System.out.println("Invalid input, please try again.");
-                    continue;
                 }
             }
         }
