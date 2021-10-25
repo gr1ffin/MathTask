3fa24dd5ac80a6c40459b2521c39d14f75b5e3cb
gr1ffin <89878211+gr1ffin@users.noreply.github.com>
2021-10-25 12:24:49 -0400

    Build 0.1.1
    Calculator file is complete :)

--- src/Algebra/Calculator.java
+++ src/Algebra/Calculator.java
@@ -1,9 +1,72 @@
 package Algebra;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Calculator {
     public void calcMethod() {
+        while(true) {
+            System.out.println("Here is how the calculator works. \nPut your first input, hit Enter. Then the sign (+,-,*,/,^), hit Enter. Finally, the second input, hit Enter. \nExample: \n1\n+\n1\nInput:\n");
+            Scanner userInput = new Scanner(System.in);
+            double inputOne = Double.parseDouble(userInput.nextLine());
+            String toDo = userInput.nextLine();
+            double inputTwo = Double.parseDouble(userInput.nextLine());
+            double answer = 0;
+            if (toDo.equals("+")) {answer = inputOne + inputTwo;}
+            else if (toDo.equals("-")) {answer = inputOne - inputTwo;}
+            else if (toDo.equals("*")) {answer = inputOne * inputTwo;}
+            else if (toDo.equals("/")) {answer = inputOne / inputTwo;}
+            else if (toDo.equals("^")) {answer = Math.pow(inputOne, inputTwo);}
+            System.out.println(answer);
+            System.out.println("Would you like to: \n- Continue with the Current Problem\n- Do a new Problem \n- Quit");
+            Scanner secondQuestion = new Scanner(System.in);
+            String continueInput = secondQuestion.nextLine().toUpperCase(Locale.ROOT);
+            if (continueInput.contains("CONTINUE")) {
+                while(true) {
+                    System.out.println("Operation first, then value. ");
+                    Scanner newInput = new Scanner(System.in);
+                    String continuedOp = newInput.nextLine();
+                    double continuedInput = Double.parseDouble(newInput.nextLine());
+                    if (continuedOp.equals("+")) {answer = answer + continuedInput;
+                        System.out.println(answer);
+                        Scanner continueAgain = new Scanner(System.in);
+                        String continuePrompt = continueAgain.nextLine().toUpperCase(Locale.ROOT);
+                        if (continuePrompt.contains("CONT")) { continue; }
+                        else { break;}}
+                    else if (continuedOp.equals("-")) {answer = answer - continuedInput;
+                        System.out.println(answer);
+                        Scanner continueAgain = new Scanner(System.in);
+                        String continuePrompt = continueAgain.nextLine().toUpperCase(Locale.ROOT);
+                        if (continuePrompt.contains("CONT")) { continue; }
+                        else { break;}}
+                    else if ((continuedOp.equals("*")) || (toDo.equals("x"))) {answer = answer * continuedInput;
+                        System.out.println(answer);
+                        Scanner continueAgain = new Scanner(System.in);
+                        String continuePrompt = continueAgain.nextLine().toUpperCase(Locale.ROOT);
+                        if (continuePrompt.contains("CONT")) { continue; }
+                        else { break;}}
+                    else if (continuedOp.equals("/")) {answer = answer / continuedInput;
+                        System.out.println(answer);
+                        Scanner continueAgain = new Scanner(System.in);
+                        String continuePrompt = continueAgain.nextLine().toUpperCase(Locale.ROOT);
+                        if (continuePrompt.contains("CONT")) { continue; }
+                        else { break;}}
+                    else if (continuedOp.equals("^")) {answer = Math.pow(answer, continuedInput);
+                        System.out.println(answer);
+                        Scanner continueAgain = new Scanner(System.in);
+                        String continuePrompt = continueAgain.nextLine().toUpperCase(Locale.ROOT);
+                        if (continuePrompt.contains("CONT")) { continue; }
+                        else { break;}}
+                    else {continue;}
+                }
+            }
+            else if (continueInput.contains("NEW")) {
+                continue;
+            }
+            else if (continueInput.contains("QUIT")) {
+                break;
 
+            }
+        }
     }
 }
--- src/main.java
+++ src/main.java
@@ -1,6 +1,10 @@
+import Algebra.Calculator;
+
 public class main {
     public static void main(String[] args) {
-        Selector se = new Selector();
-        se.selectorMethod();
+        //Selector se = new Selector();
+        //se.selectorMethod();
+        Calculator calc = new Calculator();
+        calc.calcMethod();
     }
 }
