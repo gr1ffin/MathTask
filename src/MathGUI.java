d5c6ef0f0acd2cb93200e79c5f7a92772a0061ea
gr1ffin <89878211+gr1ffin@users.noreply.github.com>
2021-11-03 14:10:30 -0400

    Volume GUI
    Finished all GUI for volume
    could make it look better, probably not though

--- src/MathGUI.java
+++ src/MathGUI.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MathGUI {
     private JPanel masterPanel;
@@ -16,14 +18,14 @@ public class MathGUI {
     private JPanel vol2;
     private JPanel vol3;
     private JTextArea advancedMathCalculatorWhatTextArea;
-    private JTextField cylinder1;
-    private JTextField cylinder2;
-    private JTextField cube1;
-    private JTextField cube2;
-    private JTextField cube3;
-    private JTextField triangle1;
-    private JTextField triangle2;
-    private JTextField triangle3;
+    private JTextField cylinderA1;
+    private JTextField cylinderA2;
+    private JTextField cubeA1;
+    private JTextField cubeA2;
+    private JTextField cubeA3;
+    private JTextField triangleA1;
+    private JTextField triangleA2;
+    private JTextField triangleA3;
     private JTabbedPane tabbedPane5;
     private JPanel sur1;
     private JPanel sur2;
@@ -39,16 +41,83 @@ public class MathGUI {
     private JTextField triangleB2;
     private JTextField triangleB3;
     private JTextField triangleB4;
-    private JTextField textField11;
-    private JTextField textField12;
+    private JTextField pyramid1;
+    private JTextField pyramid2;
     private JTextField cubeB1;
+    private JButton pyramidButton;
+    private JButton cubeButton2;
+    private JButton sphereButton;
+    private JButton cylinderButton2;
+    private JButton triangleButton;
+    private JButton cubeButton1;
+    private JButton cylinderButton1;
+    private JButton triangleButton1;
+    private JTextArea statement3;
+    private JTextArea statement1;
+    private JTextArea statement2;
+    private JTextArea sideBaseTextArea;
+    private JTextArea lengthWidthHeightTextArea2;
+    private JTextArea radiusTextArea;
+    private JTextArea radiusHeightTextArea2;
+    private JTextArea lengthHeightSideBaseTextArea;
 
+    public static String he = "Height";
+    public static String le = "Length";
+    public static String wi = "Width";
+    public static String ra = "Radius";
+    public static String ba = "Base";
+    public static String si = "Side";
+
+    public static double l = 0;
+    public static double w = 0;
+    public static double h = 0;
+    public static double r = 0;
+    public static double s = 0;
+    public static double b = 0;
+
+
+
+    public MathGUI() {
+        cubeButton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                l = Double.parseDouble(cubeA1.getText());
+                w = Double.parseDouble(cubeA2.getText());
+                h = Double.parseDouble(cubeA3.getText());
+                Methods.cubeVolume();
+                System.out.println(Methods.varOutput);
+                statement1.setText(String.valueOf(Methods.varOutput));
+            }
+        });
+        cylinderButton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                r = Double.parseDouble(cylinderA1.getText());
+                h = Double.parseDouble(cylinderA2.getText());
+                Methods.cylinderVolume();
+                System.out.println(Methods.varOutput);
+                statement2.setText(String.valueOf(Methods.varOutput));
+            }
+        });
+        triangleButton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                l = Double.parseDouble(cubeA1.getText());
+                w = Double.parseDouble(cubeA2.getText());
+                h = Double.parseDouble(cubeA3.getText());
+                Methods.triangleVolume();
+                System.out.println(Methods.varOutput);
+                statement3.setText(String.valueOf(Methods.varOutput));
+
+            }
+        });
+    }
 
     public static void main(String[] args) throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
-        MathGUI gui = new MathGUI();
+        MathGUI m = new MathGUI();
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(gui.masterPanel);
+        frame.getContentPane().add(m.masterPanel);
         frame.setSize(850, 450);
         UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
         SwingUtilities.updateComponentTreeUI(frame);
@@ -57,3 +126,7 @@ public class MathGUI {
 
     }
 }
+
+
+
+
--- src/Methods.java
+++ src/Methods.java
@@ -1,5 +1,14 @@
 public class Methods {
-    public static void cubeVolume() {
 
+    public static double varOutput = 0;
+    MathGUI m = new MathGUI();
+    public static void cubeVolume() {
+        varOutput = MathGUI.l * MathGUI.w * MathGUI.h;
+    }
+    public static void cylinderVolume() {
+        varOutput = MathGUI.h * Math.pow((MathGUI.r), 2) * Math.PI;
+    }
+    public static void triangleVolume() {
+        varOutput = 0.5 * MathGUI.l * MathGUI.w * MathGUI.h;
     }
 }
